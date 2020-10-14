ff3484d8c6a3e03128c409b48cd208a4dfa75249
Roman Andreev <anDevRom@gmail.com>
2020-10-14 09:03:36 +0300

    feat: complete human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    const numbers = [
+        ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
+        ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+      ]
+    
+      let result = ""
+    
+      if (num >= 100) {
+        result = result + numbers[0][Math.floor(num / 100)] + " hundred " + getUntil99(num - (Math.floor(num / 100) * 100))
+      } else {
+        result = result + getUntil99(num)
+      }
+    
+      function getUntil99(num) {
+        if (num < 10) {
+          result = result + numbers[0][num]
+        } else if (num >= 10 && num < 20) {
+          result = result + numbers[1][num - 10]
+        } else {
+          result = result + numbers[Math.floor(num / 10)] + " " + numbers[0][num % ((Math.floor(num / 10)) * 10)]
+        }
+    
+        return result
+      }
+
+      if (num > 0 && result.includes(" zero")) {
+          result = result.split(" zero").join("")
+      }
+
+      return result
 }
